dfec92bcbdb94ddb1ede7a7e9f6022ac8f713ee6
David Hawley Brim <davidbrim@outlook.com>
2022-08-22 14:13:39 -0500

    Build ToC from markdown pages

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,14 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverDependenciesToBundle: [
+    "mdx-bundler",
+    "rehype-slug", 
+    "hast-util-has-property", 
+    "hast-util-heading-rank",
+    "hast-util-to-string",
+    "unist-util-visit", 
+    "unist-util-visit-parents",
+    "unist-util-is"
+  ],
 };
